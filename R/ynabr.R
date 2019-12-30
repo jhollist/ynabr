7f37d0a59d1632f04522ba8605e37d941d52c940
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2019-12-29 20:17:55 -0500

    First commit

---
+++ R/ynabr.R
@@ -0,0 +1,8 @@
+#' Access the You Need A Budget API
+#' 
+#' You Need A Budget (YNAB) is budgeting software and this package provides 
+#' access to the YNAB API.  The likely use case is for users wanting to augment 
+#' the YNAB reports and graphs with custom generated ones.
+#'                  
+#' @name ynabr
+NULL 
